c92f0f6644393988f36b348a53ec168706c5b279
Blooming0 <hassanalhosyni@gmail.com>
2025-09-06 01:44:17 +0300

    every thing is uptodate

--- src/main/java/org/example/trucksy/Advice/ControllerAdvice.java
+++ src/main/java/org/example/trucksy/Advice/ControllerAdvice.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import java.sql.SQLIntegrityConstraintViolationException;
-
+//
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
     @ExceptionHandler(value = ApiException.class)
